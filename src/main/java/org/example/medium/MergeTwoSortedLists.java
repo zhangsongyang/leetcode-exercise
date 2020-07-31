0a27f6da8763ccfa589fb1e528ee3ed0ad2705d7
zhangsongyang <zhangsongyang@haier.com>
2020-07-31 23:24:37 +0800

    leetcode merge-two-sorted-lists

---
+++ src/main/java/org/example/medium/MergeTwoSortedLists.java
@@ -0,0 +1,78 @@
+package org.example.medium;
+
+/**
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ *
+ *
+ *
+ * 示例：
+ *
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MergeTwoSortedLists {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode prehead = new ListNode(-1);
+
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+
+        // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+        prev.next = l1 == null ? l2 : l1;
+
+        return prehead.next;
+    }
+
+    public static void main(String[] args) {
+        MergeTwoSortedLists mergeTwoSortedLists = new MergeTwoSortedLists();
+        ListNode listNodeA1 = new ListNode(1);
+        ListNode listNodeA2 = new ListNode(2);
+        ListNode listNodeA3 = new ListNode(4);
+        listNodeA1.next = listNodeA2;
+        listNodeA2.next = listNodeA3;
+        listNodeA3.next = null;
+
+        ListNode listNodeB1 = new ListNode(1);
+        ListNode listNodeB2 = new ListNode(3);
+        ListNode listNodeB3 = new ListNode(4);
+        listNodeB1.next = listNodeB2;
+        listNodeB2.next = listNodeB3;
+        listNodeB3.next = null;
+
+        ListNode result = mergeTwoSortedLists.mergeTwoLists(listNodeA1, listNodeB1);
+    }
+
+
+}

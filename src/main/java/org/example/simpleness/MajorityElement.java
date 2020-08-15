51fae5c5c5632f93f1652f739f21cf58c7640e69
zhangsongyang <zhangsongyang@haier.com>
2020-08-15 23:00:26 +0800

    leetcode   多数元素

---
+++ src/main/java/org/example/simpleness/MajorityElement.java
@@ -0,0 +1,38 @@
+package org.example.simpleness;
+
+import java.util.Arrays;
+
+/**
+ * 多数元素
+ * <p>
+ * 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于⌊ n/2 ⌋的元素。
+ * <p>
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ * <p>
+ * <p>
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入: [3,2,3]
+ * 输出: 3
+ * 示例2:
+ * <p>
+ * 输入: [2,2,1,1,1,2,2]
+ * 输出: 2
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/majority-element
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MajorityElement {
+
+    public static int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 2, 1, 1, 1, 2, 2};
+        System.out.println(majorityElement(nums));
+    }
+}

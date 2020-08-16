a5b23463bfdd57ba16b6106743be1407dc1f74aa
zhangsongyang <zhangsongyang@haier.com>
2020-08-16 23:10:59 +0800

    leetcode   组合

---
+++ src/main/java/org/example/medium/Combinations.java
@@ -0,0 +1,66 @@
+package org.example.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 组合
+ *
+ * <p>
+ * 给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入:n = 4, k = 2
+ * 输出:
+ * [
+ * [2,4],
+ * [3,4],
+ * [2,3],
+ * [1,2],
+ * [1,3],
+ * [1,4],
+ * ]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/combinations
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Combinations {
+
+    private List<List<Integer>> res = new ArrayList<>();
+
+    private void findCombinations(int n, int k, int begin, Stack<Integer> pre) {
+        if (pre.size() == k) {
+            // 够数了，就添加到结果集中
+            res.add(new ArrayList<>(pre));
+            return;
+        }
+        // 关键在于分析出 i 的上界
+        for (int i = begin; i <= n; i++) {
+            pre.add(i);
+            findCombinations(n, k, i + 1, pre);
+            pre.pop();
+        }
+    }
+
+
+    public List<List<Integer>> combine(int n, int k) {
+        if (n <= 0 || k <= 0 || n < k) {
+            return res;
+        }
+        findCombinations(n, k, 1, new Stack<>());
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Combinations combinations = new Combinations();
+        List<List<Integer>> result = combinations.combine(4, 2);
+        result.forEach((em) -> {
+            System.out.println(em.toString());
+        });
+    }
+
+
+}

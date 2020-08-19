d1de8391270bcc1b7bea52235dc72351b2434664
zhangsongyang <zhangsongyang@haier.com>
2020-08-19 14:03:10 +0800

    leetcode   回文子串

---
+++ src/main/java/org/example/medium/PalindromicSubstrings.java
@@ -0,0 +1,66 @@
+package org.example.medium;
+
+/**
+ * 回文子串
+ * <p>
+ * 给定一个字符串，你的任务是计算这个字符串中有多少个回文子串。
+ * <p>
+ * 具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被视作不同的子串。
+ * <p>
+ *
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入："abc"
+ * 输出：3
+ * 解释：三个回文子串: "a", "b", "c"
+ * 示例 2：
+ * <p>
+ * 输入："aaa"
+ * 输出：6
+ * 解释：6个回文子串: "a", "a", "a", "aa", "aa", "aaa"
+ *
+ * <p>
+ * 提示：
+ * <p>
+ * 输入的字符串长度不会超过 1000 。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/palindromic-substrings
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class PalindromicSubstrings {
+
+    public static Integer countSubstrings(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                System.out.println("--->" + s.substring(i, j + 1));
+                if (isPalindrome(s.substring(i, j + 1))) {
+                    System.out.println(s.substring(i, j + 1));
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "abc";
+        System.out.println(countSubstrings(s));
+    }
+
+}

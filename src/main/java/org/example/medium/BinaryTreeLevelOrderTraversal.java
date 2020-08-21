0eb273b918f6e112cf595717b4eea3efe0a7fbf1
zhangsongyang <zhangsongyang@haier.com>
2020-08-21 11:28:02 +0800

    二叉树的层序遍历

--- src/main/java/org/example/medium/BinaryTreeLevelOrderTraversal.java
+++ src/main/java/org/example/medium/BinaryTreeLevelOrderTraversal.java
@@ -1,5 +1,11 @@
 package org.example.medium;
 
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * 二叉树的层序遍历
  *
@@ -29,7 +35,125 @@ package org.example.medium;
  */
 public class BinaryTreeLevelOrderTraversal {
 
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * BFS 遍历使用队列数据结构 (这里是非层序遍历)
+     *
+     * @param root
+     * @return
+     */
+    public static List<Integer> lOrder(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            res.add(node.val);
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+        return res;
+    }
+
+    /**
+     * BFS 层序遍历
+     *
+     * @param root
+     * @return
+     */
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        if (root != null) {
+            queue.add(root);
+        }
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            res.add(level);
+        }
+
+        return res;
+    }
+
+
+    /**
+     * DFS
+     *
+     * @param root
+     * @return
+     */
+    public static List<List<Integer>> dfs(TreeNode root) {
+        List<List<Integer>> allResults = new ArrayList<>();
+        if (root == null) {
+            return allResults;
+        }
+        travel(root, 0, allResults);
+        return allResults;
+    }
+
+
+    private static void travel(TreeNode root, int level, List<List<Integer>> results) {
+        if (results.size() == level) {
+            results.add(new ArrayList<>());
+        }
+        results.get(level).add(root.val);
+        if (root.left != null) {
+            travel(root.left, level + 1, results);
+        }
+        if (root.right != null) {
+            travel(root.right, level + 1, results);
+        }
+    }
+
+
     public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        TreeNode leftOne = new TreeNode(9);
+        TreeNode rightOne = new TreeNode(20);
+        TreeNode lefttTwo = new TreeNode(15);
+        TreeNode rightTwo = new TreeNode(7);
+
+        root.left = leftOne;
+        root.right = rightOne;
+
+        rightOne.left = lefttTwo;
+        rightOne.right = rightTwo;
+
+        System.out.println(lOrder(root));
+        List<List<Integer>> lists = levelOrder(root);
+        lists.forEach((result) -> {
+            System.out.println(result.toString());
+        });
+        System.out.println("-----------------");
+        List<List<Integer>> listss = dfs(root);
+        listss.forEach((result) -> {
+            System.out.println(result.toString());
+        });
 
     }
 }

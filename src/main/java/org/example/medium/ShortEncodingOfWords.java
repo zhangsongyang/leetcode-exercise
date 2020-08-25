e3a1a1aeccee899c699f013db8da18fc6377226d
zhangsongyang <zhangsongyang@haier.com>
2020-08-25 12:34:16 +0800

    leetcode   单词的压缩编码

---
+++ src/main/java/org/example/medium/ShortEncodingOfWords.java
@@ -0,0 +1,58 @@
+package org.example.medium;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 820. 单词的压缩编码
+ *
+ * <p>
+ * 给定一个单词列表，我们将这个列表编码成一个索引字符串 S 与一个索引列表 A。
+ * <p>
+ * 例如，如果这个列表是 ["time", "me", "bell"]，我们就可以将其表示为 S = "time#bell#" 和 indexes = [0, 2, 5]。
+ * <p>
+ * 对于每一个索引，我们可以通过从字符串 S 中索引的位置开始读取字符串，直到 "#" 结束，来恢复我们之前的单词列表。
+ * <p>
+ * 那么成功对给定单词列表进行编码的最小字符串长度是多少呢？
+ * <p>
+ *  
+ * <p>
+ * 示例：
+ * <p>
+ * 输入: words = ["time", "me", "bell"]
+ * 输出: 10
+ * 说明: S = "time#bell#" ， indexes = [0, 2, 5] 。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= words.length <= 2000
+ * 1 <= words[i].length <= 7
+ * 每个单词都是小写字母 。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/short-encoding-of-words
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class ShortEncodingOfWords {
+
+    public static int minimumLengthEncoding(String[] words) {
+        Set<String> set = new HashSet<>(Arrays.asList(words));
+        for (String word : words) {
+            for (int j = 1; j < word.length(); j++) {
+                set.remove(word.substring(j));
+            }
+        }
+        int result = 0;
+        for (String word : set) {
+            result += word.length() + 1;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"time", "me", "bell"};
+        System.out.println(minimumLengthEncoding(words));
+    }
+}

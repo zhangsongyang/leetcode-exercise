f6981ec36485844e43a81c7a569ee0d1f1426330
zhangsongyang <zhangsongyang@haier.com>
2020-08-27 23:10:37 +0800

    leetcode   最大子序和

---
+++ src/main/java/org/example/simpleness/MaximumSubarray.java
@@ -0,0 +1,32 @@
+package org.example.simpleness;
+
+public class MaximumSubarray {
+
+    public static int maxSubArray(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        //定义dp数组，dp数组中的每个值dp[i]代表着以nums[i]为结尾的最大子序和
+        int[] dp = new int[n];
+        //以nums[0]结尾的最大子序和就是nums[0]
+        dp[0] = nums[0];
+        //遍历，通过状态转移方程求得dp[i]的最大子序和
+        for (int i = 1; i < n; ++i) {
+            //dp[i]的最大子序和要么是自成一派最大，要么就是当前值加上前面i - 1个数的最大子序和
+            dp[i] = Math.max(nums[i], nums[i] + dp[i - 1]);
+        }
+
+        //遍历dp数组，求得dp数组中的最大值，就是该题的答案
+        int res = Integer.MIN_VALUE;
+        for (int j = 0; j < dp.length; ++j) {
+            res = Math.max(res, dp[j]);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(maxSubArray(nums));
+    }
+}

999a8e7e94675b9d5ddb466ec22af123c516353e
zhangsongyang <zhangsongyang@haier.com>
2020-08-29 22:25:12 +0800

    leetcode    跳跃游戏 II

---
+++ src/main/java/org/example/hard/JumpGameII.java
@@ -0,0 +1,61 @@
+package org.example.hard;
+
+/**
+ *
+ * 45. 跳跃游戏 II
+ * 给定一个非负整数数组，你最初位于数组的第一个位置。
+ *
+ * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ *
+ * 你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+ *
+ * 示例:
+ *
+ * 输入: [2,3,1,1,4]
+ * 输出: 2
+ * 解释: 跳到最后一个位置的最小跳跃数是 2。
+ *      从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
+ * 说明:
+ *
+ * 假设你总是可以到达数组的最后一个位置。
+ *
+ */
+public class JumpGameII {
+
+    public static int jumpTwo(int[] nums) {
+        int position = nums.length - 1;
+        int steps = 0;
+        while (position > 0) {
+            for (int i = 0; i < position; i++) {
+                if (i + nums[i] >= position) {
+                    position = i;
+                    steps++;
+                    break;
+                }
+            }
+        }
+        return steps;
+    }
+
+
+    public static int jump(int[] nums) {
+        int length = nums.length;
+        int maxPosition = 0;
+        int step = 0;
+        int end = 0;
+
+        for (int i = 0; i < length - 1; i++) {
+            maxPosition = Math.max(maxPosition, (nums[i] + 1));
+            if (i == end) {
+                end = maxPosition;
+                step++;
+            }
+        }
+        return step;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 1, 4};
+        System.out.println(jump(nums));
+    }
+}

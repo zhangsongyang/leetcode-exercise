13aa9ae91fd20d9193b94b4e59a9b11bb11d115f
zhangsongyang <zhangsongyang@haier.com>
2020-09-23 16:04:59 +0800

    leetcode   UniquePaths

--- src/main/java/org/example/medium/UniquePaths.java
+++ src/main/java/org/example/medium/UniquePaths.java
@@ -1,21 +1,23 @@
 package org.example.medium;
 
+import java.util.Arrays;
+
 /**
  * 62. 不同路径
  * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
- *
+ * <p>
  * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
- *
+ * <p>
  * 问总共有多少条不同的路径？
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * 例如，上图是一个7 x 3 的网格。有多少可能的路径？
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: m = 3, n = 2
  * 输出: 3
  * 解释:
@@ -24,13 +26,13 @@ package org.example.medium;
  * 2. 向右 -> 向下 -> 向右
  * 3. 向下 -> 向右 -> 向右
  * 示例 2:
- *
+ * <p>
  * 输入: m = 7, n = 3
  * 输出: 28
- *
- *
+ * <p>
+ * <p>
  * 提示：
- *
+ * <p>
  * 1 <= m, n <= 100
  * 题目数据保证答案小于等于 2 * 10 ^ 9
  */
@@ -46,13 +48,24 @@ public class UniquePaths {
         }
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                dp[i][j] = dp[i-1][j]+dp[i][j-1];
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
-        return dp[m-1][n-1];
+        return dp[m - 1][n - 1];
+    }
+
+    public static int uniquePathsTwo(int m, int n) {
+        int[] cur = new int[n];
+        Arrays.fill(cur, 1);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                cur[j] += cur[j - 1];
+            }
+        }
+        return cur[n - 1];
     }
 
     public static void main(String[] args) {
-        System.out.println(uniquePaths(7, 3));
+        System.out.println(uniquePathsTwo(7, 3));
     }
 }

28d3258898a4f04d8e52d5b3a90289f4bf44f957
zhangsongyang <zhangsongyang@haier.com>
2020-11-03 00:20:14 +0800

    leetcode   234. 回文链表

--- src/main/java/org/example/simpleness/PalindromeLinkedList.java
+++ src/main/java/org/example/simpleness/PalindromeLinkedList.java
@@ -40,6 +40,49 @@ public class PalindromeLinkedList {
         }
     }
 
+    /**
+     * 快慢指针解法
+     *
+     * @param head
+     * @return
+     */
+    public static boolean isPalindromeTwo(ListNode head) {
+        ListNode fast = head, slow = head;
+        //通过快慢指针找到中点
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        //如果fast不为空，说明链表的长度是奇数个
+        if (fast != null) {
+            slow = slow.next;
+        }
+        //反转后半部分链表
+        slow = reverse(slow);
+
+        fast = head;
+        while (slow != null) {
+            //然后比较，判断节点值是否相等
+            if (fast.val != slow.val)
+                return false;
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+
+    //反转链表
+    public static ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
 
     public static boolean isPalindrome(ListNode head) {
         List<Integer> vals = new ArrayList<Integer>();
@@ -73,6 +116,7 @@ public class PalindromeLinkedList {
         listNodeB.next = listNodeC;
         listNodeC.next = listNodeD;
         System.out.println(isPalindrome(listNodeA));
+        System.out.println(isPalindromeTwo(listNodeA));
     }
 
 

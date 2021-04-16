d4dc6d42a8a1bc76bd98e551623322370001a249
zhangsongyang <zhangsongyang@haier.com>
2021-04-16 17:14:18 +0800

    剑指 Offer 22. 链表中倒数第k个节点

---
+++ src/main/java/org/example/simpleness/LianBiaoZhongDaoShuDiKgeJieDianLcof.java
@@ -0,0 +1,59 @@
+package org.example.simpleness;
+
+/**
+ * 剑指 Offer 22. 链表中倒数第k个节点
+ * 输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
+ *
+ * 例如，一个链表有 6 个节点，从头节点开始，它们的值依次是 1、2、3、4、5、6。这个链表的倒数第 3 个节点是值为 4 的节点。
+ *
+ *
+ *
+ * 示例：
+ *
+ * 给定一个链表: 1->2->3->4->5, 和 k = 2.
+ *
+ * 返回链表 4->5.
+ *
+ * https://leetcode-cn.com/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof/
+ */
+public class LianBiaoZhongDaoShuDiKgeJieDianLcof {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        int k = 2;
+        ListNode result = getKthFromEnd(head, k);
+        while (result !=null){
+            System.out.println(result.val);
+            result = result.next;
+        }
+
+    }
+
+    public static ListNode getKthFromEnd(ListNode head, int k) {
+        ListNode slow = head;
+        ListNode fast = head;
+        for (int i = 0; i < k; i++) {
+            fast = fast.next;
+        }
+        while (fast != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+}

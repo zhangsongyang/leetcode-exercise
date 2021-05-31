fac57cd9918f68a68fe732fe6b6dec5af16b6c6e
zhangsongyang <328429080@qq.com>
2021-05-31 08:54:07 +0800

    master

--- src/main/java/org/example/simpleness/TwoSum.java
+++ src/main/java/org/example/simpleness/TwoSum.java
@@ -54,10 +54,22 @@ public class TwoSum {
         TwoSum twoSum = new TwoSum();
         int[] nums = {2, 7, 11, 15};
         int target = 9;
-        int[] result = twoSum.twoSumB(nums, target);
+        int[] result = twoSum.twoSumC(nums, target);
         for (int i : result) {
             System.out.println(i);
         }
 
+    }
+
+    public int[] twoSumC(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap();
+        for(int i=0; i< nums.length; i++) {
+            int result = target - nums[i];
+            if(map.containsKey(result)){
+                return new int[]{i, map.get(result)};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
--- src/main/java/org/example/simpleness/ValidParentheses.java
+++ src/main/java/org/example/simpleness/ValidParentheses.java
@@ -74,9 +74,25 @@ public class ValidParentheses {
         String s = "()[]{}";
         String ss = "(]";
         String sss = "";
-        System.out.println(isValid(s));
-        System.out.println(isValid(ss));
-        System.out.println(isValid(sss));
+        System.out.println(isValidTwo(s));
+        System.out.println(isValidTwo(ss));
+        System.out.println(isValidTwo(sss));
+    }
+
+    public static boolean isValidTwo(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if(c == '(') {
+                stack.push(')');
+            } else if(c == '[') {
+                stack.push(']');
+            } else if(c == '{') {
+                stack.push('}');
+            } else if(stack.isEmpty() ||stack.pop() != c){
+                return false;
+            }
+        }
+        return stack.isEmpty();
     }
 
 }
